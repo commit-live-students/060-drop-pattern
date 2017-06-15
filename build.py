0934d53488aaa91c28fbf68a5ede5e46564e85e4
RupaliHangekar <RupaliHangekar@users.noreply.github.com>
2017-06-15 18:45:33 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,19 @@
 def solution():
-    """Enter Code Here"""
+    ans=''
+    for row in range(0,7):
+        for column in range(0,5):
+            if(
+            ((row==0 or row==6) and (column==0 or column==4))
+            or
+            ((column==1 or column==2 or column==3) and (row==1 or row==2 or row==4 or row==5))
+            or
+            (column==1 and row==3)
+            or
+            (column==4 and row==2)
+            ):
+                ans=ans+' '
+            else:
+                ans=ans+'*'
+        ans=ans+'\n'
+    print ans
+solution()
