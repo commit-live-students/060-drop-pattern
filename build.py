2e70a2e1d5a2e63843cd9a8e8c5dd3fe57471f8d
datanomnom <ronzzz92@gmail.com>
2017-06-20 17:24:14 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,10 @@
 def solution():
     """Enter Code Here"""
+    a=" *** "
+    b="*   *"
+    c="*    "
+    d="* ***"
+    e="*   *"
+    f="*   *"
+    g=" *** "
+    return a +"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f+"\n"+g
